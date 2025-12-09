1e8d807c7b0d8b8b019ca8a0f877479d4633f080
Esteban Sanchez <>
2025-12-09 04:35:51 -0500

    console log para revisar

--- marketplace/server/src/server.js
+++ marketplace/server/src/server.js
@@ -20,7 +20,7 @@ const app = express();
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
-
+console.log("pasamos el inicio")
 app.use('/api/auth', authRoutes);
 app.use("/api/chat", conversationRoutes);
 app.use('/api', productRoutes);
@@ -29,7 +29,7 @@ app.use('/api/admin', adminRoutes);
 app.get("/", (_req, res) => res.send("API OK"));
 
 const server = http.createServer(app);
-
+console.log("creamos rutas y http")
 const io = new Server(server, {
   cors: {
     origin: FRONTEND_URL,
@@ -37,7 +37,7 @@ const io = new Server(server, {
     credentials: true,
   },
 });
-
+console.log("creamos io con url de front")
 // función helper para generar un room único para 2 usuarios
 const getRoomId = (userA, userB) => {
   const ids = [String(userA), String(userB)].sort();
